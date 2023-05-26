7c500e4048238a274527ed5b5a6f6fc55f9e2264
Lawrence Godfrey <lawrence.godfrey@kazang.com>
2023-05-26 22:31:30 +0200

    Create general utils package.

---
+++ internal/utils/utils.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"path/filepath"
+	"runtime"
+)
+
+// GetRootDir Returns the full path to the root directory of the project.
+func GetRootDir() string {
+	// Get the full path to this file.
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		panic("No caller information")
+	}
+
+	// Get the directory of this file.
+	thisDir := filepath.Dir(filename)
+
+	// Get the directory of the models directory, which is two directories up.
+	rootDir := filepath.Dir(filepath.Dir(thisDir))
+
+	return rootDir
+}
+
+// GetModelsDir Returns the full path to the `models` directory.
+func GetModelsDir() string {
+	// Get the directory of the models directory by joining the root directory and "models".
+	rootDir := GetRootDir()
+	ModelsDir := filepath.Join(rootDir, "models")
+
+	return ModelsDir
+}

d41906291b2c8914384382d7abcfb22a1a695a21
Lawrence Godfrey <lawrence.godfrey@kazang.com>
2023-05-28 13:34:23 +0200

    Implement the Term Frequency-Inverse Document Frequency (TF-IDF) algorithm for basic keyword extraction

--- internal/processing/keyword_extraction.go
+++ internal/processing/keyword_extraction.go
@@ -1,46 +1,71 @@
 package processing
 
 import (
-	"context"
-	"github.com/nlpodyssey/cybertron/pkg/tasks"
-	"github.com/nlpodyssey/cybertron/pkg/tasks/tokenclassification"
-	"text_summarizer/internal/utils"
+	"math"
+	"sort"
 )
 
-type Classifier struct {
-	model tokenclassification.Interface
+// Term represents a term's frequency in a piece of text.
+//
+// Term: the term (word) itself.
+// Count: the number of times the term appears in the text.
+// Freq: the frequency of the term in the text.
+// InverseDocumentFrequency: the frequency of the term normalized by the number of documents.
+type Term struct {
+	Term                     string
+	Count                    int
+	Freq                     float64
+	InverseDocumentFrequency float64
 }
 
-func (c *Classifier) ExtractKeywords(text string) ([]tokenclassification.Token, error) {
-	return extractKeywords(text, c.model)
-}
+// TermFrequencies returns a slice of structs containing the term frequencies of the given text,
+// sorted by frequency.
+func TermFrequencies(words []string) []*Term {
 
-func LoadModel() (*Classifier, error) {
-	model, err := tasks.Load[tokenclassification.Interface](&tasks.Config{
-		ModelsDir: utils.GetModelsDir(),
-		ModelName: "dslim/bert-base-NER",
+	frequencies := make(map[string]*Term)
+
+	for _, w := range words {
+		if _, ok := frequencies[w]; !ok {
+			frequencies[w] = &Term{
+				Term:  w,
+				Count: 1,
+			}
+		} else {
+			frequencies[w].Count++
+		}
+	}
+
+	// Convert map to slice.
+	sortedFrequencies := make([]*Term, 0, len(frequencies))
+	for _, frequency := range frequencies {
+		sortedFrequencies = append(sortedFrequencies, frequency)
+	}
+
+	// Sort the slice by Count.
+	sort.Slice(sortedFrequencies, func(i, j int) bool {
+		return sortedFrequencies[i].Count > sortedFrequencies[j].Count
 	})
 
-	if err != nil {
-		return nil, err
+	// Calculate the frequency of each term.
+	for _, frequency := range sortedFrequencies {
+		frequency.Freq = float64(frequency.Count) / float64(len(words))
 	}
 
-	defer tasks.Finalize(model)
-
-	return &Classifier{model: model}, nil
+	return sortedFrequencies
 }
 
-func extractKeywords(text string, model tokenclassification.Interface) ([]tokenclassification.Token, error) {
-
-	params := tokenclassification.Parameters{
-		AggregationStrategy: tokenclassification.AggregationStrategySimple,
+// InverseDocumentFrequency returns a slice of structs containing the
+// inverse document frequency of the given term frequencies, sorted by IDF.
+func InverseDocumentFrequency(terms []*Term, numDocuments int) []*Term {
+	// Calculate the IDF of each term.
+	for _, term := range terms {
+		term.InverseDocumentFrequency = math.Log(float64(numDocuments) / float64(term.Count))
 	}
 
-	result, err := model.Classify(context.Background(), text, params)
+	// Sort the slice by IDF.
+	sort.Slice(terms, func(i, j int) bool {
+		return terms[i].InverseDocumentFrequency > terms[j].InverseDocumentFrequency
+	})
 
-	if err != nil {
-		return []tokenclassification.Token{}, err
-	}
-
-	return result.Tokens, nil
+	return terms
 }

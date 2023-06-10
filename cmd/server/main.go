186167e0f56750f6f98295ff8bea0d9d418ef4a7
Lawrence Godfrey <lawrence.godfrey@kazang.com>
2023-06-10 15:07:03 +0200

    Create a main server script to serve the gRPC API

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,22 @@
-package server
+package main
+
+import (
+	"google.golang.org/grpc"
+	"net"
+	"text_summarizer/api/proto"
+	"text_summarizer/internal/api"
+)
+
+func main() {
+	s := grpc.NewServer()
+	proto.RegisterSummarizerServer(s, &api.Server{})
+
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := s.Serve(lis); err != nil {
+		panic(err)
+	}
+}
